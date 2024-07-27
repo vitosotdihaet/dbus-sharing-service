21f476f59fdb5ff640faac10fc92d197558b875a
Vitaly Klimenko <vitostikitos@gmail.com>
2024-07-28 02:46:21 +0300

    Create a SharingService class

---
+++ sharingService.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QMap>
+#include <QDBusError>
+
+class SharingService : public QObject
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "com.system.sharing")
+
+public slots:
+    void RegisterService(const QString &name, const QStringList &supportedFormats);
+    void OpenFile(const QString &path);
+    void OpenFileUsingService(const QString &path, const QString &service);
+
+private:
+    QMap<QString, QStringList> services;
+};

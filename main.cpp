3bf935786f04e756870afd3fb4fc538f1d229c57
Vitaly Klimenko <vitostikitos@gmail.com>
2024-07-28 15:04:10 +0300

    Remove useless nullptr parent passed into the constructor

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
-    SharingService sharingService(nullptr);
+    SharingService sharingService;
 
     return app.exec();
 }

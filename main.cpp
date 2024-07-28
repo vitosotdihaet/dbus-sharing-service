a03cb2acb7ddcd142803ba50a083727bdb167d6f
Vitaly Klimenko <vitostikitos@gmail.com>
2024-07-28 14:39:31 +0300

    Use nullptr, instead of creating a dummy parent QObject

--- main.cpp
+++ main.cpp
@@ -7,23 +7,7 @@
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
-    SharingService sharingService;
-
-    QDBusConnection dbusConnection = QDBusConnection::sessionBus();
-    if (!dbusConnection.isConnected()) {
-        qFatal("Can't connect to the D-Bus session bus.");
-        return 1;
-    }
-
-    if (!dbusConnection.registerService("com.system.sharing")) {
-        qFatal("Can't register D-Bus service.");
-        return 1;
-    }
-
-    if (!dbusConnection.registerObject("/", &sharingService, QDBusConnection::ExportAllSlots)) {
-        qFatal("Can't register D-Bus object.");
-        return 1;
-    }
+    SharingService sharingService(nullptr);
 
     return app.exec();
 }
